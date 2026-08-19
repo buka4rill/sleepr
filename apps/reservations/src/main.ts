c2d3e6ebe3c96c723e868b8fcdbe2136e2fc8d96
Ebuka <buka4rill@gmail.com>
2026-08-19 10:38:02 +0200

    chore: added pino logger

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+
+  // validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // logger
+  app.useLogger(app.get(Logger));
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
