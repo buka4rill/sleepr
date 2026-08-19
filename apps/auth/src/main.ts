7112a0f2a317a03ea0d77a4a38c5692cff066969
Ebuka <buka4rill@gmail.com>
2026-08-19 13:28:46 +0200

    chore: added pino logger to auth service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { Logger } from 'nestjs-pino/Logger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
