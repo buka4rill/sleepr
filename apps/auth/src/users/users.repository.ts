e8bbf80c48d75d39b23448e7028de7b24ff2f619
Ebuka <buka4rill@gmail.com>
2026-08-19 13:12:56 +0200

    chore: added auth app with users module

---
+++ apps/auth/src/users/users.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { UserDocument } from './models/users.schema';
+import { AbstractRepository } from '@app/common/database/abstract.repository';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+  constructor(
+    @InjectModel(UserDocument.name)
+    userModel: Model<UserDocument>,
+  ) {
+    super(userModel);
+  }
+}
