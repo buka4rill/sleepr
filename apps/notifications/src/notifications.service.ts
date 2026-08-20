43a3a49419136bff7d21dddbb8da2372ef72601d
Ebuka <buka4rill@gmail.com>
2026-08-20 15:43:20 +0200

    chore: added google smtp

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+import { Transporter } from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
+  private readonly transporter: Transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAUTH2',
+        user: this.configService.get('SMTP_USER'),
+        clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+        clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+        refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+      },
+    });
+  }
+
   async notifyEmail({ email }: NotifyEmailDto) {
-    console.log(email);
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr Notification',
+      text: 'You have a new notification.',
+    });
   }
 }
