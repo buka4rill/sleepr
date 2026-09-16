97bdb99d346d283e2cdc7cb157b19ba2611b7f23
Ebuka <buka4rill@gmail.com>
2026-09-16 13:47:31 +0200

    chore: e2e done

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,4 +1,6 @@
 describe('Reservations', () => {
+  let jwt: string;
+
   beforeAll(async () => {
     const user = {
       email: 'sleeprnestapp@gmail.com',
@@ -20,11 +22,58 @@ describe('Reservations', () => {
         'Content-Type': 'application/json',
       },
     });
-    const jwt = await response.text();
-    console.log(jwt);
+    jwt = await response.text();
   });
 
-  test('Create', () => {
-    expect(true).toBeTruthy();
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+      },
+    );
+
+    const reservation = (await responseGet.json()) as unknown as {
+      _id: string;
+    };
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '08/22/2023',
+          endDate: '02/12/2024',
+          placeId: '13347',
+          invoiceId: '143125',
+          charge: {
+            amount: 20,
+            card: {
+              token: 'tok_visa',
+            },
+          },
+        }),
+      },
+    );
+
+    expect(responseCreate.ok).toBeTruthy();
+
+    const createdReservation = (await responseCreate.json()) as unknown as {
+      _id: string;
+    };
+
+    return createdReservation;
+  };
 });
